63782ca99cfe0d0378b8e47d8ac39bb2efe4ee62
Duke Pan <matthewdukepan@gmail.com>
2024-10-14 21:37:41 -0700

    Added tabs + formatting

--- extensions/pearai-inventory/webview-ui/src/App.tsx
+++ extensions/pearai-inventory/webview-ui/src/App.tsx
@@ -1,18 +1,46 @@
 import InventoryPage from "./pages/InventoryPage";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+const TemplateComponent = ({ name }: { name: string }) => {
+	return (
+		<div className="flex items-center justify-center h-screen text-7xl">
+			{name} here
+		</div>
+	);
+};
+
+const tabs = [
+	{
+        id: "aider",
+		name: "Creator (aider)",
+		component: <TemplateComponent name="aider" />,
+    },
+    { id: "inventory", name: "Inventory", component: <InventoryPage /> },
+    {
+        id: "perplexity",
+        name: "Search (Perplexity)",
+        component: <TemplateComponent name="Perplexity" />,
+    },
+];
+
 export default function App() {
 	return (
 		<div className="h-full">
-			<Tabs defaultValue="inventory">
-				<TabsList>
-					<TabsTrigger value="inventory">Inventory</TabsTrigger>
-					<TabsTrigger value="perplexity">Perplexity</TabsTrigger>
+			<Tabs defaultValue="inventory" className="">
+            <div className="flex justify-center mt-1">
+				<TabsList className="bg-input text-center text-xs">
+					{tabs.map((tab) => (
+						<TabsTrigger key={tab.id} value={tab.id}>
+							{tab.name}
+						</TabsTrigger>
+					))}
 				</TabsList>
-				<TabsContent value="inventory">
-					<InventoryPage />
-				</TabsContent>
-				<TabsContent value="perplexity">Perplexity here.</TabsContent>
+                </div>
+				{tabs.map((tab) => (
+					<TabsContent key={tab.id} value={tab.id} className="pb-4 pl-4 pr-4">
+						{tab.component}
+					</TabsContent>
+				))}
 			</Tabs>
 		</div>
 	);
