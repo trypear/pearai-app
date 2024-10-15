58939ae6bbdd23d0e962a0ff336703cb754e919a
Duke Pan <matthewdukepan@gmail.com>
2024-10-14 18:59:00 -0700

    Add tabs

--- extensions/pearai-inventory/webview-ui/src/App.tsx
+++ extensions/pearai-inventory/webview-ui/src/App.tsx
@@ -1,9 +1,19 @@
 import InventoryPage from "./pages/InventoryPage";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function App() {
-    return (
-        <div className="h-full">
-            <InventoryPage />
-        </div>
-    );
+	return (
+		<div className="h-full">
+			<Tabs defaultValue="inventory">
+				<TabsList>
+					<TabsTrigger value="inventory">Inventory</TabsTrigger>
+					<TabsTrigger value="perplexity">Perplexity</TabsTrigger>
+				</TabsList>
+				<TabsContent value="inventory">
+					<InventoryPage />
+				</TabsContent>
+				<TabsContent value="perplexity">Perplexity here.</TabsContent>
+			</Tabs>
+		</div>
+	);
 }
